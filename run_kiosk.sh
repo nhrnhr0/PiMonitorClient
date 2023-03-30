767ff72561853e54f23279d862276804aa8b9b5d
nhrnhr0 <nhrnhr0@gmail.com>
2023-03-30 14:35:58 +0000

    working on odroid

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -16,8 +16,8 @@ xset -dpms
 
 unclutter -idle 0.5 -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/odroid/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/odoird/.config/chromium/Default/Preferences
 
 
 website_url=`cat url.txt`
@@ -25,7 +25,7 @@ echo '================================================='
 echo "$website_url"
 echo '================================================='
 # Launch the browser in full screen Kiosk mode 
-sudo -u pi /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk $website_url &
+sudo -u odroid /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk $website_url &
 
 
 echo "waiting for the process to start"
--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 # env setup
+sudo apt install python3.8-venv -y
 python3 -m venv env
 source ./env/bin/activate
 pip install -r requirements.txt
 sudo cp prod.env .env
 
 # supervisor
-sudo apt update -y && sudo apt install supervisor -y
+sudo apt update
+sudo apt install supervisor -y
 sudo apt install unclutter -y
 sudo mkdir /var/log/gunicorn
 sudo apt install cec-utils
